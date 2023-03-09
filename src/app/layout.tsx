488009833fa3972d49d8610094445855f3a1908c
Tim Rice <timrice@gmail.com>
2023-03-09 16:06:59 -0500

    moved providers component in root layout to only wrap children

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,25 +13,25 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
-        <Providers>
-          <div className="bg-gray-100 py-3 mb-5 shadow">
-            <header className="flex justify-between items-center container mx-auto max-w-4xl px-2 lg:px-0">
-              <Link href="/" className="font-black tracking-tighter text-3xl">
-                RSC Demo
-              </Link>
-              <nav className="flex gap-5">
-                {links.map(({ href, text }) => (
-                  <Link key={href} href={href} className="text-blue-500 hover:underline">
-                    {text}
-                  </Link>
-                ))}
-              </nav>
-            </header>
-          </div>
-          <div className="container mx-auto max-w-4xl px-2 lg:px-0">
-            <main>{children}</main>
-          </div>
-        </Providers>
+        <div className="bg-gray-100 py-3 mb-5 shadow">
+          <header className="flex justify-between items-center container mx-auto max-w-4xl px-2 lg:px-0">
+            <Link href="/" className="font-black tracking-tighter text-3xl">
+              RSC Demo
+            </Link>
+            <nav className="flex gap-5">
+              {links.map(({ href, text }) => (
+                <Link key={href} href={href} className="text-blue-500 hover:underline">
+                  {text}
+                </Link>
+              ))}
+            </nav>
+          </header>
+        </div>
+        <div className="container mx-auto max-w-4xl px-2 lg:px-0">
+          <main>
+            <Providers>{children}</Providers>
+          </main>
+        </div>
       </body>
     </html>
   )
